bee9735df047114048cadf4301d3cfd7da4e6b70
qi <xbg@maqi.me>
2015-12-04 00:26:18 +0800

    something

---
+++ bpool/bpool.go
@@ -0,0 +1,38 @@
+package bpool
+
+import "bytes"
+
+type SizedBufferPool struct {
+	c chan *bytes.Buffer
+	a int
+}
+
+func NewSizedBufferPool(size, alloc int) *SizedBufferPool {
+	return &SizedBufferPool{
+		c: make(chan *bytes.Buffer, size),
+		a: alloc,
+	}
+}
+
+func (bp *SizeBufferPool) Get() (b *byte.Buffer) {
+	select {
+	case b = <-bp.c:
+	default:
+		b = bytes.NewBuffer(make([]byte, 0, bp.a))
+	}
+
+	return
+}
+
+func (bp *SizeBufferPool) Put(b *bytes.Buffer) {
+	b.Reset()
+
+	if b.Cap() > bp.a {
+		b = bytes.NewBuffer(make([]byte, 0, bp.a))
+	}
+
+	select {
+	case bp.c <- b:
+	default:
+	}
+}
---
+++ netstring/netstring.go
@@ -0,0 +1,16 @@
+package netstring
+
+/*
+utf-8
+<31 32 3a 68 65 6c 6c 6f 20 77 6f 72 6c 64 21 2c>
+12:hello world!,
+<30 3a 2c>
+0:,
+*/
+func Marshall(org []byte) []byte {
+	return nil
+}
+
+func Unmarshall(ns []byte) []byte {
+	return nil
+}
